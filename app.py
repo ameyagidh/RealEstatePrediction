ce5c3c9eeab505ba8c7898cac1d62ac243299073
ameyagidh <ameyagidh2@gmail.com>
2023-10-04 10:09:19 -0400

    done

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def home():
     return render_template("home.html")
 
 @app.route("/predict_api",methods=["POST"])
-def predict():
+def predict_api():
     data = request.json["data"]
     print(data)
     print("Ameya")
@@ -25,6 +25,13 @@ def predict():
     output = model.predict(new_data)
     print(output[0])
     return jsonify(output[0])
+
+@app.route("/predict",methods=["POST"])
+def predict():
+    data = [float(x) for x in request.form.values()]
+    final_input = scalar.transform(np.array(data).reshape(1,-1))
+    output = model.predict(final_input)[0]
+    return render_template("home.html",prediction_text="The House price prediction is {}".format(output))
 
 if __name__=="__main__":
     app.run(debug=True)
