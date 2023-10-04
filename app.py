19a0aaee999d76e890e75d2f4851033d444f56c2
ameyagidh <ameyagidh2@gmail.com>
2023-10-04 09:59:07 -0400

    done

--- app.py
+++ app.py
@@ -8,14 +8,23 @@ import pandas as pd
 app=Flask(__name__)
 
 ## Load the model
-regmodel=pickle.load(open('regmodel.pkl','rb'))
+model=pickle.load(open('regmodel.pkl','rb'))
+scalar = pickle.load(open("scalar.pkl","rb"))
+
 @app.route('/')
 def home():
 
     return render_template("home.html")
 
-@app.route("/predict",methods=["POST"])
+@app.route("/predict_api",methods=["POST"])
 def predict():
     data = request.json["data"]
     print(data)
-    return render_template("")
+    print("Ameya")
+    new_data = scalar.transform(np.array(list(data.values())).reshape(1,-1))
+    output = model.predict(new_data)
+    print(output[0])
+    return jsonify(output[0])
+
+if __name__=="__main__":
+    app.run(debug=True)
