98a7cf519734bcce84cf547f18d80155c302d3c1
ameyagidh <ameyagidh2@gmail.com>
2023-10-04 10:09:56 -0400

    done

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def predict():
     data = [float(x) for x in request.form.values()]
     final_input = scalar.transform(np.array(data).reshape(1,-1))
     output = model.predict(final_input)[0]
-    return render_template("home.html",prediction_text="The House price prediction is {}".format(output))
+    return render_template("home.html",prediction_text="Price Predicted is {}".format(output))
 
 if __name__=="__main__":
     app.run(debug=True)
