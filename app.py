bca18b518df8ccc65cc0b2a4c8e31c2e2cf662c5
ameyagidh <ameyagidh2@gmail.com>
2023-10-04 09:34:48 -0400

    done

--- app.py
+++ app.py
@@ -1,2 +1,21 @@
-import flask
+import json
 import pickle
+
+from flask import Flask,request,app,jsonify,url_for,render_template
+import numpy as np
+import pandas as pd
+
+app=Flask(__name__)
+
+## Load the model
+regmodel=pickle.load(open('regmodel.pkl','rb'))
+@app.route('/')
+def home():
+
+    return render_template("home.html")
+
+@app.route("/predict",methods=["POST"])
+def predict():
+    data = request.json["data"]
+    print(data)
+    return render_template("")
